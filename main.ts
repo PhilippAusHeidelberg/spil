235dc75fffe05c78d1a8076fe7e55f1fe49968ed
PhilippAusHeidelberg <110686148+PhilippAusHeidelberg@users.noreply.github.com>
2022-09-18 14:29:46 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     if (philipp.get(LedSpriteProperty.X) == 2) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
         basic.showIcon(IconNames.SmallSquare, 70)
         basic.showIcon(IconNames.Square, 70)
+        basic.pause(5000)
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -15,6 +23,6 @@ let philipp: game.LedSprite = null
 philipp = game.createSprite(2, 2)
 basic.forever(function () {
     philipp.move(1)
-    basic.pause(1000)
+    basic.pause(100)
     philipp.ifOnEdgeBounce()
 })
