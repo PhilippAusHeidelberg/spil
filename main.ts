1e3437819525a212e5f9618a2e2b8872ac449062
PhilippAusHeidelberg <110686148+PhilippAusHeidelberg@users.noreply.github.com>
2022-09-18 14:23:55 +0200

    Knopf A funzjonirt

--- main.ts
+++ main.ts
@@ -1,19 +1,20 @@
 input.onButtonPressed(Button.A, function () {
-    game.addScore(1)
-    philipp.delete()
-    basic.pause(500)
     if (philipp.get(LedSpriteProperty.X) == 2) {
-        philipp = game.createSprite(2, 2)
-        philipp.delete()
-    }
-    if (philipp.isDeleted()) {
-        philipp = game.createSprite(2, 2)
+        basic.showIcon(IconNames.SmallSquare, 70)
+        basic.showIcon(IconNames.Square, 70)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
     }
 })
 let philipp: game.LedSprite = null
 philipp = game.createSprite(2, 2)
 basic.forever(function () {
     philipp.move(1)
-    basic.pause(100)
+    basic.pause(1000)
     philipp.ifOnEdgeBounce()
 })
